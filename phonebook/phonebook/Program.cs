c5de14ea0ae31b8861a5f4e1f879d1cbd629c5fc
Alinka150601 <alina.bartsevich@mail.ru>
2021-11-18 14:16:18 +0300

    Revert "Revert "add crud app""
    This reverts commit be15bea3f34fce71ae63e286a108f55bd21e6e30.

---
+++ phonebook/phonebook/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace phonebook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var menu = new Menu();
+            menu.PrintMenu();
+        }
+    }
+}
